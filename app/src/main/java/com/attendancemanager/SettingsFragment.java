37c8f81497406ddda2767f57b7cc6f1f28d7a3a7
Krithic Kumar <krithickumarub@gmail.com>
2020-09-13 00:50:25 +0530

    updated settings fragment

--- app/src/main/java/com/attendancemanager/SettingsFragment.java
+++ app/src/main/java/com/attendancemanager/SettingsFragment.java
@@ -9,10 +9,15 @@ import android.widget.ListView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.preference.PreferenceFragmentCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
+import github.com.st235.lib_expandablebottombar.ExpandableBottomBar;
+
 public class SettingsFragment extends PreferenceFragmentCompat {
+
+    private RecyclerView recyclerView;
 
     private static final String TAG = "SettingsFragment";
 
@@ -23,17 +28,34 @@ public class SettingsFragment extends PreferenceFragmentCompat {
 
     @Override
     public RecyclerView onCreateRecyclerView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
-        return super.onCreateRecyclerView(inflater, parent, savedInstanceState);
+        recyclerView = super.onCreateRecyclerView(inflater, parent, savedInstanceState);
+        return recyclerView;
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        final ListView lv = view.findViewById(android.R.id.list);
     }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+
+        recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            ExpandableBottomBar bottomBar = getActivity().findViewById(R.id.bottom_bar);
+
+            @Override
+            public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+
+                if (dy > 0) {
+                    /* Scrolled down */
+                    bottomBar.hide();
+                } else {
+                    /* Scrolled up */
+                    bottomBar.show();
+                }
+            }
+        });
     }
 }
