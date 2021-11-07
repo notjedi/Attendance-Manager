9cf6d03a0de6ff6f462669719631960c07a53084
jedi2610 <krithickumarub@protonmail.com>
2021-11-08 01:48:56 +0530

    just to keep repo updated

--- app/src/main/java/com/attendancemanager/view/MainActivity.java
+++ app/src/main/java/com/attendancemanager/view/MainActivity.java
@@ -40,8 +40,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setTheme(R.style.AppTheme);
         setContentView(R.layout.activity_main);
+        setTheme(R.style.AppTheme);
 
         /* Find views */
         mBottomBar = findViewById(R.id.bottom_bar);
